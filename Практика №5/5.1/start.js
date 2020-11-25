e123f2e7ba3d18b4e2d0ff7407813d559e54c144
Dmitriy886 <70742326+Dmitriy886@users.noreply.github.com>
2020-11-25 14:21:36 +0300

    Create start.js

---
+++ Практика №5/5.1/start.js
@@ -0,0 +1,13 @@
+let num = 3;
+
+alert(num);
+
+let a = 10, b = 2;
+
+alert(`Сумма a + b = ${a + b}\nРазность a - b = ${a - b}\nПроизведение a * b = ${a * b}\nЧастное a / b = ${a / b}`);
+
+let c = 15, d = 2;
+
+let result = c + d;
+
+alert(result);
