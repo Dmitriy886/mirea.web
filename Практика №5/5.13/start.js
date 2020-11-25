bf481111e0b990f98ddcef9a4312ac36f48979c5
Dmitriy886 <70742326+Dmitriy886@users.noreply.github.com>
2020-11-25 14:45:27 +0300

    Create start.js

---
+++ Практика №5/5.13/start.js
@@ -0,0 +1,17 @@
+num = 1;
+
+switch (num) {
+    case 1:
+        result = "Зима";
+        break;
+    case 2:
+        result = "Весна";
+        break;
+    case 3:
+        result = "Лето";
+        break;
+    case 4:
+        result = "Осень";
+}
+
+alert(result);
