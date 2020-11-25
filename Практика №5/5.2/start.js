82dcb8a0bf9baffed20a84366af22682120d1c3d
Dmitriy886 <70742326+Dmitriy886@users.noreply.github.com>
2020-11-25 14:25:22 +0300

    Create start.js

---
+++ Практика №5/5.2/start.js
@@ -0,0 +1,15 @@
+let str = 'Привет, Мир!';
+
+alert(str);
+
+let str1 = 'Привет, ', str2 = 'Мир!';
+
+alert(str1 + str2);
+
+let name = 'Дмитрий';
+
+alert(`Привет, ${name}!`);
+
+let age = 19;
+
+alert(`Мне ${age} лет!`);
