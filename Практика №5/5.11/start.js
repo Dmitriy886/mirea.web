52552b2383807ddb6d999435d13678e4a2c7546f
Dmitriy886 <70742326+Dmitriy886@users.noreply.github.com>
2020-11-25 14:43:18 +0300

    Create start.js

---
+++ Практика №5/5.11/start.js
@@ -0,0 +1,31 @@
+let test = true;
+
+/* Длинная запись */
+
+if (test == true) 
+    alert('Верно')
+else 
+    alert('Неверно');
+
+
+/* Короткая запись */
+
+if (test) 
+    alert('Верно') 
+else 
+    alert('Неверно');
+
+/* Длинная запись */
+
+if (test != true) 
+    alert('Верно')
+else 
+    alert('Неверно');
+
+
+/* Короткий вариант */
+
+if (!test) 
+    alert('Верно') 
+else 
+    alert('Неверно');
